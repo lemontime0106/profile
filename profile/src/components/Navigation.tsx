e70e6e41fd7d0ac83f41ea4584ff55eaf9b2d808
SSAFY10kim <ehddud0534@gmail.com>
2024-06-30 23:28:53 +0900

    feat: 초기설정

---
+++ profile/src/components/Navigation.tsx
@@ -0,0 +1,25 @@
+//라이브러리
+import React from "react";
+
+//css 파일
+
+//file
+
+const Navigation: React.FC = () => {
+  return (
+    <>
+      <div className="flex justify-between m-[1%]">
+        <div>
+          <p className="font-bold text-2xl">KDY's Portfolio</p>
+        </div>
+        <div className="flex min-w-[30%] justify-around items-center">
+          <p>About me</p>
+          <p>Skills</p>
+          <p>Project</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navigation;
